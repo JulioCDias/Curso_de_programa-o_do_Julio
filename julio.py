d5e48a9dc7d47c9370bcde9401bca3a33b9d7d83
JulioCDias <juliozulu2008@gmail.com>
2024-11-10 13:00:32 -0300

    inicio do Curso

--- julio.py
+++ julio.py
@@ -1,5 +1,33 @@
 xpto = "dhsjfhejkfhejwkf"
 print(xpto)
 
+"""
+Os tipos de dados em Python são:
+Inteiros: Representados por int, são elementos do conjunto matemático de inteiros, positivos e negativos 
+ 
+Ponto flutuante: Representados por float, são números com parte decimal 
+ 
+Complexo: Representados por complex, são números complexos como um par de números de ponto flutuante 
+ 
+Strings: Representados por str, são armazenados na classe str 
+ 
+Booleanos: Representados por bool 
+ 
+Listas: Representadas por list 
+ 
+Tuplas: Representadas por tuple 
+ 
+Dicionários: Representados por dict 
+ 
+Sets: Representados por set 
+ 
+Frozenset: Representado por frozenset 
+ 
+Bytes: Representados pela classe bytes, são usados para armazenar dados binários 
+ 
+Bytearray: Representado pela classe bytearray, são usados para armazenar dados binários 
+
+"""
+
 
 
